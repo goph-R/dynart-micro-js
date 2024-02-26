4e8ab48672ae18cd16ece2dd7b7cfd0a800a9aad
gopher <gopher.hu@gmail.com>
2024-02-26 13:52:44 +0100

    [WIP] Improve DynamicList

--- dynamic-list.js
+++ dynamic-list.js
@@ -1,114 +1,183 @@
 const DynamicListColumnView = {
-    Text: function(item, property) {
+    rowCheckboxCounter: 0,
+    text: function(item, property) {
         return item[property]
     },
-    NonBreakingText: function(item, property) {
+    nonBreakingText: function(item, property) {
         return item[property].replaceAll(' ', '&nbsp;')
     },
-    Check: function(item, property) {
+    check: function(item, property) {
         return item[property] ? '&check;' : ''
     },
-    DateTime: function(item, property) {
+    dateTime: function(item, property) {
         const d = new Date(item[property])
         return d.toLocaleString()
     }
 }
 
-const DynamicList = function(selector, options) {
+const DynamicList = function($container, options) {
 
-    const element = $(selector).get()
+    const that = this
 
-    const $defaultForm = $('<form>')
-    $defaultForm.html(`
-        <input type="hidden" name="limitOffset" value="0">
-        <input type="hidden" name="limitMax" value="100">
-        <input type="hidden" name="orderBy" value="id">
-        <input type="hidden" name="orderDir" value="desc">
-    `)
+    const defaultFormHtml = `
+        <form style="display: none">
+            <div>
+                <input type="hidden" name="order" value="">
+                <input type="hidden" name="sort" value="asc">
+                <input type="hidden" name="offset" value="0">
+                <input type="hidden" name="max" value="1000">
+            </div>
+        </form>
+    `
 
-    // create layout
-    $(element).html(`
-        <table>
-            <thead>
-                <tr>
-                    <th class="checkbox-column"><input type="checkbox"></th>                    
-                    <th class="actions-column"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="template">
-                    <td class="checkbox-column"><input type="checkbox"></td>
-                    <td class="actions-column">
-                        <ul class="action-list row-actions">
-                            <li class="template"><button><i class="fa-solid"></i><span></span></button></li>
-                        </ul>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="pagination">
-            <button class="prev-button"><i class="fa-solid fa-caret-left"></i><span></span></button>
-            <button class="next-button"><i class="fa-solid fa-caret-right"></i><span></span></button>
-            <ul>
-                <li class="template"><a></a></li>
-            </ul>
+    $container.html(options.$filterForm ? '' : defaultFormHtml + `
+        <div class="dynamic-list col-md-12 list grid-body table-responsive" style="padding: 0">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="sortable checkbox-column" style="padding: 8px 8px 8px 10px"><input type="checkbox"></th>
+                        <th class="actions-column"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="template">
+                        <td class="checkbox-column"><input type="checkbox"></td>
+                        <td class="actions-column">
+                            <ul class="action-list row-actions">
+                                <li class="template"><span class="button icon"><a class="edit"></a></span></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="no-results">No results</div>
+            <div class="paging bottom row" style="margin-right:0;">
+                <div style="col-xs-6">
+                    <ul class="customPaginationRight" style="float:right; padding-right:0; text-align: right">
+                        <a href="#" class="prevLink">Previous</a>
+                        <a href="#" class="nextLink">Next</a>
+                    </ul>
+                </div>
+                <div class="col-xs-6 customPagination" style="float:left">
+                    Records 1-20 from 40
+                </div>
+            </div>
         </div>
     `)
 
-    // set options
     const idProperty = options.idProperty || 'id'
     const columnViews = options.columnViews || {}
     const groupActions = options.groupActions || null
     const rowActions = options.rowActions || []
-    const $filterForm = options.$filterForm || $defaultForm
     const findItems = options.findItems || function(filters) { return { items: [], total: 0 } }
-    const defaultTexts = {
-        prev: 'Prev',
-        next: 'Next'
-    }
-    const texts = options.texts ? $.extend({}, defaultTexts, options.texts) : defaultTexts
+    const $filterForm = options.$filterForm ? options.$filterForm : $container.find('form')
+
+    const $noResults = $container.find('.no-results')
+    const $table = $container.find('table')
+    const $tbody = $container.find('tbody')
+    const $rowTemplate = $tbody.find('tr')
+    const $rowActionTemplate = $tbody.find('.row-actions li')
+    const $headCheckbox = createHeaderCheckbox();
+
+    const $paging = $container.find('.paging')
+    const $prevLink = $paging.find('.prevLink')
+    const $nextLink = $paging.find('.nextLink')
+    const $pages = $paging.find('.customPaginationRight')
+    const $recordsLabel = $paging.find('.customPagination')
+
+    const $maxInput = $filterForm.find('input[name="max"]')
+    const $offsetInput = $filterForm.find('input[name="offset"]')
+    const $orderInput = $filterForm.find('input[name="order"]')
+    const $sortInput = $filterForm.find('input[name="sort"]')
 
     let $rowCheckboxes = []
+    let refreshDoneCallback = null
 
-    const $body = $(element).find('tbody')
-    const $rowTemplate = $body.find('tr')
-    const $rowActionTemplate = $body.find('.row-actions li')
-    const $prevButton = $(element).find('.prev-button')
-    const $nextButton = $(element).find('.next-button')
-    $prevButton.find('span').text(texts.prev)
-    $nextButton.find('span').text(texts.next)
-
-    // create columns
-    let $prevHeader = $(element).find('th.actions-column')
-    let $prevCell = $body.find('td.actions-column')
-    Object.entries(columnViews).forEach(function (entry) {
-
-        const [property, columnView] = entry
-
-        const $header = $('<th>')
-        $header.attr('data-property', property)
-        $header.html(DynamicListColumnView.NonBreakingText(columnView, 'label'))
-        $header.insertAfter($prevHeader)
-        $prevHeader = $header
-
-        const $cell = $('<td>')
-        $cell.attr('data-property', property)
-        if (columnView.align) {
-            $cell.css('text-align', columnView.align)
-        }
-        $cell.insertAfter($prevCell)
-        $prevCell = $cell
-    })
-
-    // init group actions
-    if (groupActions) {
-        groupActions.forEach(function (groupAction) {
-            groupAction.$button.click(function() {
-                groupAction.action(selectedIds())
-            })
+    this.refresh = function(doneCallback = null) {
+        refreshDoneCallback = doneCallback // TODO: better solution
+        const filters = {}
+        const inputs = $filterForm.serializeArray()
+        inputs.forEach(function (input) {
+            filters[input.name] = input.value
         })
-    } else {
-        $(element).find('.checkbox-column').each(function (i, e) { $(e).hide() })
+        findItems(filters, refreshCallback)
+    }
+
+
+    createColumns();
+    initGroupActions();
+    initRowActions();
+
+    $prevLink.click(pageLinkClick)
+    $nextLink.click(pageLinkClick)
+
+    this.refresh()
+
+    function pageLinkClick() {
+        setCurrentPage($(this).attr('data-page'))
+        that.refresh()
+    }
+
+    function getPageSize() {
+        return parseInt($maxInput.attr('value'))
+    }
+
+    function getCurrentPage() {
+        return parseInt($offsetInput.attr('value')) / getPageSize()
+    }
+
+    function setCurrentPage(page) {
+        $offsetInput.attr('value', page * getPageSize())
+    }
+
+    function createHeaderCheckbox() {
+        const $headCheckbox = $container.find('th.checkbox-column input')
+        $headCheckbox.change(function () {
+            if ($headCheckbox.prop('checked')) {
+                $rowCheckboxes.forEach(function ($cb) {
+                    $cb.prop('checked', true)
+                })
+            } else if (isAllChecked()) {
+                $rowCheckboxes.forEach(function ($cb) {
+                    $cb.prop('checked', false)
+                })
+            }
+            adjustGroupActionButtons()
+        })
+        return $headCheckbox;
+    }
+
+    function initRowActions() {
+        if (rowActions.length) {
+            $container.find('.actions-column').show()
+        } else {
+            $container.find('.actions-column').hide()
+        }
+    }
+
+    function createColumns() {
+        // create columns
+        let $prevHeader = $container.find('th.checkbox-column')
+        let $prevCell = $tbody.find('td.checkbox-column')
+        Object.entries(columnViews).forEach(function (entry) {
+
+            const [property, columnView] = entry
+
+            const $header = $('<th>')
+            $header.attr('data-property', property)
+            $header.html(DynamicListColumnView.nonBreakingText(columnView, 'label'))
+            $header.insertAfter($prevHeader)
+            $prevHeader = $header
+
+            const $cell = $('<td>')
+            $cell.attr('data-property', property)
+            if (columnView.align) {
+                $cell.css('text-align', columnView.align)
+            }
+            $cell.insertAfter($prevCell)
+            $prevCell = $cell
+
+        })
     }
 
     function createFromTemplate($templateElement) {
@@ -120,57 +189,119 @@ const DynamicList = function(selector, options) {
         return result
     }
 
-    function refreshCallback(result) {
+    function initGroupActions() {
+        // init group actions
+        if (groupActions) {
+            groupActions.forEach(function (groupAction) {
+                groupAction.$button.click(function () {
+                    groupAction.action(selectedIds())
+                })
+            })
+        } else {
+            $container.find('.checkbox-column').each(function (i, e) {
+                $(e).hide()
+            })
+        }
+    }
 
-        // clear rows
-        $body.find('tr').each(function (i, e) {
+    function refreshCallback(result) {
+        clearContent();
+        adjustPaging(result)
+        createContent(result);
+        adjustGroupActionButtons()
+        if (typeof refreshDoneCallback === 'function') {
+            refreshDoneCallback()
+        }
+    }
+
+    function clearContent() {
+        $tbody.find('tr').each(function (i, e) {
             if (!$(e).hasClass('template')) {
                 $(e).remove()
             }
         })
         $rowCheckboxes = []
+    }
 
-        // add rows
-        result.items.forEach(function (item) {
-
-            const $row = createFromTemplate($rowTemplate)
-
-            // add checkbox
-            const $rowCheckbox = $row.find('input[type=checkbox]')
-            $rowCheckbox.attr('value', item[idProperty])
-            $rowCheckbox.change(adjustGroupActionButtons)
-            $rowCheckboxes.push($rowCheckbox)
-
-            // add row actions
-            const $rowActions = $row.find('.row-actions')
-            rowActions.forEach(function (rowAction) {
-                const $rowAction = createFromTemplate($rowActionTemplate)
-                const $button = $rowAction.find('button')
-                const $icon = $rowAction.find('i')
-                const $label = $rowAction.find('span')
-                $button.click(function() {
-                    rowAction.action(item[idProperty])
-                })
-                $icon.addClass(rowAction.icon)
-                $label.text(rowAction.label)
-                $rowActions.append($rowAction)
-            })
-
-            // add property cells
-            Object.entries(item).forEach(function (entry) {
-                const [property, value] = entry
-                if (!columnViews[property]) {
-                    return
+    function createContent(result) {
+        if (result.items.length) {
+            $noResults.hide()
+            const pageSize = getPageSize()
+            const currentPage = getCurrentPage()
+            for (let i = 0; i < pageSize; i++) {
+                const index = currentPage * pageSize + i
+                if (index > result.items.length - 1) {
+                    break
                 }
-                const view = columnViews[property].view || DynamicListColumnView.Text
-                const $cell = $row.find('td[data-property="' + property + '"]')
-                $cell.html(view(item, property))
-            })
+                addItem(result.items[i], i)
+            }
+            $table.show()
+        } else {
+            $table.hide()
+            $noResults.show()
+        }
+    }
 
-            $body.append($row)
+    function addItem(item, indexOnPage) {
+        const $row = createFromTemplate($rowTemplate)
+        const rowCheckboxId = addRowCheckbox($row, item);
+        $row.addClass(indexOnPage % 2 === 0 ? 'odd' : 'even')
+        addRowActions($row, item);
+        Object.keys(columnViews).forEach(function (property) {
+            addCell(property, $row, rowCheckboxId, item);
         })
+        $tbody.append($row)
+    }
 
-        adjustGroupActionButtons()
+    function addRowCheckbox($row, item) {
+        const $rowCheckbox = $row.find('input[type=checkbox]')
+        const rowCheckboxCounter = ++DynamicListColumnView.rowCheckboxCounter
+        const rowCheckboxId = 'row_checkbox_' + rowCheckboxCounter + '_' + item[idProperty]
+        $rowCheckbox.attr('value', item[idProperty])
+        $rowCheckbox.change(adjustGroupActionButtons)
+        $rowCheckbox.attr('id', rowCheckboxId)
+        $rowCheckboxes.push($rowCheckbox)
+        return rowCheckboxId;
+    }
+
+    function addRowActions($row, item) {
+        const $rowActions = $row.find('.row-actions')
+        rowActions.forEach(function (rowAction) {
+            const $rowAction = createFromTemplate($rowActionTemplate)
+            const $button = $rowAction.find('a')
+            $button.click(function () {
+                rowAction.action(item[idProperty])
+            })
+            $button.addClass(rowAction.type)
+            $button.attr('title', rowAction.title)
+            $rowActions.append($rowAction)
+        })
+    }
+
+    function addCell(property, $row, rowCheckboxId, item) {
+        const view = columnViews[property].view || DynamicListColumnView.text
+        const viewOptions = columnViews[property].options || {}
+        const $cell = $row.find('td[data-property="' + property + '"]')
+        const $label = $('<label>')
+        $label.attr('for', rowCheckboxId)
+        $label.html(view(item, property, viewOptions))
+        addEventsForItem($label, viewOptions);
+        $cell.html('')
+        $cell.append($label)
+    }
+
+    function addEventsForItem($label, viewOptions) {
+        const $element = $label.find('[data-use-events]:first')
+        if (!$element) {
+            return
+        }
+        Object.entries(viewOptions).forEach(function (entry) {
+            const [k, v] = entry
+            if (typeof v === 'function' && k.startsWith('on')) {
+                const lowerCaseEventWithoutOn = k.slice(2).toLowerCase()
+                $element.on(lowerCaseEventWithoutOn, v)
+            }
+        })
     }
 
     function selectedIds() {
@@ -183,24 +314,98 @@ const DynamicList = function(selector, options) {
         return result
     }
 
+    function isAllChecked() {
+        if (!$rowCheckboxes.length) {
+            return false
+        }
+        for (let i = 0; i < $rowCheckboxes.length; i++) {
+            if (!$rowCheckboxes[i].is(':checked')) {
+                return false
+            }
+        }
+        return true
+    }
+
     function adjustGroupActionButtons() {
         let anyChecked = false
         $rowCheckboxes.forEach(function ($checkbox) {
             anyChecked |= $checkbox.is(':checked')
         })
-        groupActions.forEach(function (groupAction) {
-            groupAction.$button.prop('disabled', !anyChecked)
-        })
+        if (groupActions) {
+            groupActions.forEach(function (groupAction) {
+                const $b = groupAction.$button
+                $b.prop('disabled', !anyChecked)
+                $b.removeClass('btn-danger')
+                if (anyChecked) {
+                    $b.addClass('btn-danger')
+                }
+            })
+        }
+        $headCheckbox.prop('checked', isAllChecked())
     }
 
-    this.refresh = function() {
-        const filters = {}
-        const inputs = $filterForm.serializeArray()
-        inputs.forEach(function (input) {
-            filters[input.name] = input.value
-        })
-        findItems(filters, refreshCallback)
-    }
+    function adjustPaging(result) {
 
-    this.refresh()
+        // adjust pages
+        const total = result.total ? result.total : result.items.length
+        const pageSize = getPageSize()
+
+        let currentPage = getCurrentPage()
+
+        if (total <= pageSize) {
+            // hide if no paging needed
+            $pages.hide()
+        } else {
+            // set total
+            let totalPages = Math.ceil(total / pageSize)
+            if (isNaN(totalPages) || !isFinite(totalPages)) {
+                totalPages = 0
+                console.error('Total pages is not a number or not finite')
+            }
+            if (currentPage > totalPages - 1) {
+                currentPage = totalPages - 1
+                setCurrentPage(currentPage)
+            }
+            // remove old page links
+            $pages.find('.step').remove()
+            $pages.find('.currentStep').remove()
+            // add new page links
+            for (let i = 0; i < totalPages; i++) {
+                const $pageLink = $('<a>')
+                $pageLink.attr('data-page', i)
+                if (i == currentPage) {
+                    $pageLink.addClass('currentStep')
+                } else {
+                    $pageLink.addClass('step')
+                }
+                $pageLink.html(i + 1)
+                $pageLink.click(pageLinkClick)
+                $prevLink.insertAfter($pageLink)
+            }
+            // show paging
+            $pages.show()
+        }
+
+        // adjust prev-next links
+        if (currentPage === 0) {
+            $prevLink.hide()
+        } else {
+            $prevLink.attr('data-page', currentPage - 1)
+            $prevLink.show()
+        }
+        if (currentPage === total - 1) {
+            $nextLink.hide()
+        } else {
+            $nextLink.attr('data-page', currentPage + 1)
+            $nextLink.show()
+        }
+
+        // adjust records label
+        const pageStart = currentPage * pageSize + 1
+        let pageEnd = currentPage * pageSize + pageSize
+        if (pageEnd > total) {
+            pageEnd = total
+        }
+        $recordsLabel.html('Records ' + pageStart + '-' + pageEnd + ' from ' + total)
+    }
 }
