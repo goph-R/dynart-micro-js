5c33ca18c03b90222796730da30448380ef116db
Gábor László <gopher.hu@gmail.com>
2025-06-10 16:40:49 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -3,9 +3,7 @@ const UI = new function() {
     const that = this
     const $container = $('#ui_message')
 
-    $container.find('.close').click(function() {
-        that.hideMessage()
-    })
+    let timeout = null
 
     this.scrollToTop = function(top = 0, duration = 0) {
         $("html, body").stop().animate({ scrollTop: top }, duration)
@@ -17,8 +15,12 @@ const UI = new function() {
         } else {
             $container.removeClass('errors')
         }
-        $container.html(message)
+        $container.html(message + '<span class="close">&times;</span>')
+        $container.find('.close').click(that.hideMessage)
         $container.show()
+        if (timeout) {
+            clearTimeout(timeout)
+        }
     }
 
     this.hideMessage = function() {
@@ -28,5 +30,18 @@ const UI = new function() {
 
     this.showError = function(message) {
         that.showMessage(message, true)
+    }
+
+    this.showFetchError = function(url, params, result) {
+        if (result.status === 401) {
+            that.showError('You are not authorized to perform this action. (Maybe login session expired?)')
+            return
+        }
+        let errorMsg = 'Error happened while fetching ' + url
+        if (params != null) {
+            errorMsg += ' with parameters: ' + JSON.stringify(params)
+        }
+        errorMsg += "\n" + JSON.stringify(result)
+        that.showError(errorMsg)
     }
 }
